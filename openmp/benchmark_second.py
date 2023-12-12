d503554b355ab56d0fb3b8ec29abe51034eb70e2
martin1724 <martinmatyja1724@gmail.com>
2023-12-12 01:52:01 +0100

    next push

--- openmp/benchmark_second.py
+++ openmp/benchmark_second.py
@@ -6,19 +6,24 @@ import os
 
 
 input_strings = [
-    #random.choice(string.printable)*10**1, #  10
+    random.choice(string.printable)*10**1, #  10
     random.choice(string.printable)*10**2, #  100
-    random.choice(string.printable)*10**3, #  1000
-    #random.choice(string.printable)*10**4, #  10000
-    #random.choice(string.printable)*10**5, #  100000
-    #random.choice(string.printable)*2*10**5,# 200000
-    #random.choice(string.printable)*5*10**5,# 500000
-    #random.choice(string.printable)*10**6,  # 1000000
-    #random.choice(string.printable)*2*10**6,# 2000000
-    #random.choice(string.printable)*5*10**6,# 5000000
+    random.choice(string.printable)*10**3, #  1 000
+    random.choice(string.printable)*10**4, #  10 000
+    random.choice(string.printable)*10**5, #  100 000
+    random.choice(string.printable)*2*10**5,# 200 000
+    random.choice(string.printable)*5*10**5,# 500 000
+    random.choice(string.printable)*10**6,  # 1 000 000
+    random.choice(string.printable)*2*10**6,# 2 000 000
+    random.choice(string.printable)*5*10**6,# 5 000 000
+    random.choice(string.printable)*1*10**7, # 10 000 000
+    random.choice(string.printable)*2*10**7, # 20 000 000
+    random.choice(string.printable)*5*10**7, #50 000 000
+    random.choice(string.printable)*8*10**7, # 80 000 000
+    random.choice(string.printable)*1*10**8 # 100 000 000
 ]
 
-NUMBER_OF_TEST_CASES = 10
+NUMBER_OF_TEST_CASES = 1
 
 your_program = "build/porr_project"  # Replace with the actual path to your Python program
 
--- openmp/charts_threads.py
+++ openmp/charts_threads.py
@@ -8,20 +8,35 @@ import matplotlib.pyplot as plt
 """
 
 
-x_values = [1, 2, 3, 4]
+x_values = [5, 6, 8, 10]
 
-y_values_list = [247549.5, 174395.4, 189840.3, 407933.4 ]
+y_values_list = [
+    [152094601.0, 166405878.0, 224550263.0, 266364262.0], # 100mkn
+    [120590306.0, 134713111.0, 191275419.0, 211001291.0],  # 80 mln
+    [75657528.0, 84138649.0, 117310365.0, 141898394.0], # 50 mln
+    [30538599.0, 32692948.0, 43636105.0, 63411144.0], # 20 mln
+    [15166572.0, 16725476.0, 21449213.0, 30330893.0] # 10 mln
+]
 
 plt.figure(figsize=(8, 6))
 
-plt.scatter(x_values, y_values_list, s=100, label='Serie 1', marker='o', linestyle='-', color='red')
+plt.scatter(x_values, y_values_list[0], s=100, label='Serie 1', marker='o', linestyle='-', color='red')
+plt.scatter(x_values, y_values_list[1], s=100, label='Serie 1', marker='o', linestyle='-', color='green')
+plt.scatter(x_values, y_values_list[2], s=100, label='Serie 1', marker='o', linestyle='-', color='blue')
+plt.scatter(x_values, y_values_list[3], s=100, label='Serie 1', marker='o', linestyle='-', color='c')
+plt.scatter(x_values, y_values_list[4], s=100, label='Serie 1', marker='o', linestyle='-', color='#e377c2')
 
 plt.xlabel('Liczba wątków')
-plt.ylabel('Średni czas wykonania szyfrowania (długość wiadomości 100000) w sekundach [s]')
+plt.ylabel('Średni czas wykonania szyfrowania w sekundach [s]')
 plt.title('Wykres średniego czasu trwania szyfrowania od liczby wątków')
 plt.xticks(x_values, x_values)
 
-plt.legend(['OpenMP', 'Seq'])
+plt.legend(['100mln', '80mln', '50mln', '20mln', '10mln'])
 plt.grid(True)
+plt.plot(x_values, y_values_list[0], color='red')
+plt.plot(x_values, y_values_list[1], color='green')
+plt.plot(x_values, y_values_list[2], color='blue')
+plt.plot(x_values, y_values_list[3], color='c')
+plt.plot(x_values, y_values_list[4], color='#e377c2')
 
 plt.show()
